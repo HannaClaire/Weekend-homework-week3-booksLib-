50b4e8842c309878f0541f12c58537c5a704d442
Hanna <hannadurham3@gmail.com>
2023-04-22 18:48:24 +0100

    further_updated_version

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,21 +1,51 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
-from models.books import books 
+from models.books import *
 from models.book import *
 
 @app.route('/books')
 def index():
     return render_template('index.html', title="Home", books=books)
 
-@app.route('/books/<int:book_id>')
-def show_book(book_id):
-    book = books
-    return render_template('specific_book.html', book = book)
+
+@app.route('/books', methods = ['POST'])
+def add_book():
+    title = request.form['title']
+    author = request.form['author']
+    genre = request.form['genre']
+    new_book= Book(title, author, genre)
+    add_new_book(new_book)
+    books = show_all_books()
+    return render_template('index.html', title="Home", books=books)
+
+
+@app.route("/books/<index>")  # this will return the details of a chosen book
+def show_selected_book(index):
+    return render_template("specific_book.html", book=books[int(index)])
+
+@app.route('/books/delete/<title>', methods=['POST'])
+def delete(title):
+    delete_book(title)
+    return redirect('/books')
+
+
+
+    # add_new_book(new_book)
+    # # add redirect here instead of render
+    # return render_template('index.html', title='Home', books=books)
+
+
+
+
+
+# @app.route('/books/<int:book_id>')
+# def show_book(book_id):
+#     book = books
+#     return render_template('specific_book.html', book = book)
 
 
 # @app.route('/books/index')
 # def specific_book(index):
-
-#     book = books[int(index)-1]
-#     return render_template('book.html', book = book,)
+#     book = books[int(index)]
+#     return render_template('book.html', book = book)
 
--- models/books.py
+++ models/books.py
@@ -6,3 +6,17 @@ book3 = Book("The Northern Lights", "Philip Pullman", "Fantasy")
 
 books = [book1, book2, book3]
 
+def add_new_book(book):
+    books.append(book)
+
+def show_all_books():
+    print(books)
+    return books
+
+def delete_book(book_title):
+    book_to_delete = None
+    for book in books:
+        if book.title == book_title:
+            book_to_delete = book
+            break
+    books.remove(book_to_delete)
